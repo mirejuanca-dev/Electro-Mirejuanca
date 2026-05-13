50268ec9380918fc3949339b16c1769974194443
mirejuanca-dev <mirejuanca@gmail.com>
2026-05-13 16:46:09 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Service Worker para permitir el funcionamiento de la PWA
+self.addEventListener('fetch', function(event) {
+    // Por ahora, solo cumple con el requisito de instalación
+    // En el futuro, aquí añadiremos el código para que la app funcione sin internet
+});
