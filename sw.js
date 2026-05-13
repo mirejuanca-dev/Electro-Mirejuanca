b376c03158f915fb04a95bc328574bbb8da5e11f
mirejuanca-dev <mirejuanca@gmail.com>
2026-05-13 17:10:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// Service Worker para permitir el funcionamiento de la PWA
+// Service Worker para PWA
 self.addEventListener('fetch', function(event) {
-    // Por ahora, solo cumple con el requisito de instalación
-    // En el futuro, aquí añadiremos el código para que la app funcione sin internet
+    // Vacío por ahora para compatibilidad básica
 });
